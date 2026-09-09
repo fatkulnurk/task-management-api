961ef471ab49d1c9453ce9765de3f0aeaabdbd4d
fatkulnurk <fatkulnurk@gmail.com>
2026-09-10 04:27:04 +0700

    platform: notification logger

---
+++ internal/platform/notification/logger.go
@@ -0,0 +1,24 @@
+package notification
+
+import (
+	"context"
+	"log/slog"
+	applicationnotification "taskmanagement/internal/application/notification"
+)
+
+type Logger struct {
+	Logger *slog.Logger
+}
+
+func NewLogger(logger *slog.Logger) *Logger {
+	return &Logger{Logger: logger}
+}
+
+func (notificationLogger *Logger) Send(ctx context.Context, notification applicationnotification.Notification) error {
+	notificationLogger.Logger.InfoContext(ctx, "task notification",
+		"user_id", notification.UserID,
+		"task_id", notification.TaskID,
+		"message", notification.Message,
+	)
+	return nil
+}
