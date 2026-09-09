035a21c5fad4ae3236a6c54bcdd4529b4fc8a1f8
fatkulnurk <fatkulnurk@gmail.com>
2026-09-10 06:54:16 +0700

    auth: http requests

---
+++ internal/modules/auth/adapter/api/request.go
@@ -0,0 +1,20 @@
+package api
+
+type RegisterRequest struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type RefreshRequest struct {
+	RefreshToken string `json:"refresh_token"`
+}
+
+type LogoutRequest struct {
+	RefreshToken string `json:"refresh_token"`
+}
